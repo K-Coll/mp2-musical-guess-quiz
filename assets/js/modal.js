7020a740b38540068aabb7ce67d60ff1aff4a737
Katie Collins <katie93collins@gmail.com>
2024-06-03 12:37:31 +0000

    chore: add comments to JS code for How to Play modal to explain and aid understanding

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,11 +1,16 @@
+// How To Play Rules Modal 
+
+// Delcare variables
 const modal = document.querySelector('#modal');
 const openModal = document.querySelector('.open-button');
 const closeModal = document.querySelector('.close-button');
 
+// Open Modal
 openModal.addEventListener('click', () => {
     modal.showModal();
 })
 
+// Close Modal
 closeModal.addEventListener('click', () => {
     modal.close();
 })
